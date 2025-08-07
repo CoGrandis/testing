8e29b765558c7f9f347d467c1ae83bdad7333777
CoGrandis <grandisconstanza@gmail.com>
2025-08-07 07:48:37 -0300

    users endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,24 @@
 import express from "express";
 const port:number=3000
 const app = express();
+app.use(express.json());
 app.listen(port,()=>{
     console.log(`Servidor corriendo en el puerto ${port}`);
+})
+
+
+app.get("/users", (req, res)=>{
+    res.json(
+    [{
+        id:1,
+        username:"maria",
+        email:"maria@gmail.com.ar",
+    },
+    {
+        id:2,
+        username:"maria2",
+        email:"maria2@gmail.com.ar",
+    }])
 })
 
 app.use("/users/:id", (req, res)=>{
