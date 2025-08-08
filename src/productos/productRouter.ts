882e77d13aeaa75f64e4824539efbd051d9ab2b1
CoGrandis <grandisconstanza@gmail.com>
2025-08-08 09:13:42 -0300

    create endpoint

--- src/productos/productRouter.ts
+++ src/productos/productRouter.ts
@@ -1,6 +1,7 @@
 import express, { Router } from "express";
 import { ProductService } from "./services/productServices";
 import { IProductRepository } from "./repository/productRepository";
+import { Product } from "./model/productModel";
 
 abstract class AbstractRouter{
     protected router:Router;
@@ -21,7 +22,21 @@ export class ProductRouter extends AbstractRouter{
            const result =  await this.productService.getAll()
            res.json(result)
         })
-        this.router.get("/:id", this.productService.getProductById)
+        this.router.get("/:id", async (req, res)=>{
+            const id:number= parseInt(req.params.id)
+            const result =  await this.productService.getProductById(id)
+            res.json(result)
+        })
+        this.router.post("/", async(req, res) => {
+            const {name, code, price} = req.body
+            const producto:Product={
+                code:code,
+                name:name,
+                price:price
+            }
+            const result = await this.productService.createProduct(producto)
+            res.json(result)
+        })
     }
 
 }
